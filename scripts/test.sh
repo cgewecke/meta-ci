46fd80bf9b8cc79ecf71dc016e184c534c3eb76b
cgewecke <christophergewecke@gmail.com>
2018-03-11 23:28:41 -0700

    Fix env comparisons

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,15 +2,15 @@
 npm run build
 chmod +x build/cli.bundled.js
 
-if [ "$ZEPPELIN" = true ]; then
+if [ "$ZEPPELIN" == true ]; then
   cd targets/zeppelin-solidity
   npm test
   cd ../..
-elif [ "$ARAGON" = true]; then
+elif [ "$ARAGON" == true ]; then
   cd targets/aragonOS
   npm test
   cd ../..
-elif [ "$COLONY" = true]; then
+elif [ "$COLONY" == true ]; then
   cd targets/colonyNetwork
   npm run test:contracts
   cd ../..
